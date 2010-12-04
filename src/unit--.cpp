2899a3c0270ed763513195fbcc39dbbdc1d40d69
gcross <gcross@phys.washington.edu>
2010-12-04 13:57:33 -0500

    Moved license messages into separate nodes.

--- src/unit--.cpp
+++ src/unit--.cpp
@@ -2,23 +2,28 @@
 //@+node:gcross.20090119092241.4: * @thin unit--.cpp
 //@@language cplusplus
 //@@tabwidth -4
+
+//@+<< License >>
+//@+node:gcross.20101203210234.1954: ** << License >>
+//@+at
 // unit--, a simple and easy-to-use unit test aid for C++
 // Copyright (C) 2005~2006  Tsong Chong
 // birdiez@126.com
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+//     This program is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program; if not, write to the Free Software
+//     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//@-<< License >>
 
 #include <iostream>
 #include <string>
